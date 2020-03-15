c09234c89bda5d84ecb4252aac4f471c87552d45
kumata-san <kumatasan2019@gmail.com>
2020-03-15 12:03:56 +0900

    renamed build to docs

---
+++ docs/precache-manifest.9782d599621d8b64b7d7679a3238fbee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d12428aa193ec1539a54a5087e3c3c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0438fcb47ff2c14707e",
+    "url": "/static/js/2.3a67b91c.chunk.js"
+  },
+  {
+    "revision": "a5288a4ed4e4beba8413aa5c9ae969ea",
+    "url": "/static/js/2.3a67b91c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d5df534b344d59d9d4d",
+    "url": "/static/js/main.d59eaf59.chunk.js"
+  },
+  {
+    "revision": "45e99d1abd7bff9052c8",
+    "url": "/static/js/runtime-main.fa9479e2.js"
+  }
+]);
